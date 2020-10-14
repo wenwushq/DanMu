18f21cdc996b59a480fd77beeaacd75d049a7632
wenwushq <41138799+wenwushq@users.noreply.github.com>
2020-10-14 11:06:05 +0800

    Update DanMu.cpp

--- DanMu/DanMu.cpp
+++ DanMu/DanMu.cpp
@@ -1,6 +1,8 @@
 #include "DanMu.h"
 #include <QTimer>
 #include "DanMuControl.h"
+#include <QHBoxLayout>
+#include <QLabel>
 
 DanMu::DanMu(QWidget *parent)
     : QWidget(parent)
@@ -43,21 +45,27 @@ DanMu::~DanMu()
 void DanMu::slotGenAnimation()
 {
 	int nIndex = 0;
+	QList<QWidget*> lstWidgets;
 	for each (QString strDanMu in m_strlstDanMus)
 	{
-		QLabel *pLabel = new QLabel(strDanMu, this);
+		QWidget *pWidget = new QWidget(this);
+		QHBoxLayout *pHLayout = new QHBoxLayout();
+		pHLayout->setMargin(0);
+		pWidget->setLayout(pHLayout);
+		QLabel *pLabel = new QLabel(strDanMu, pWidget);
 		nIndex = nIndex >= m_strColors.count() ? 0 : nIndex;
 		pLabel->setStyleSheet(m_strColors[nIndex]);
 		nIndex++;
-		pLabel->move(0, -500);
 		pLabel->setVisible(true);
 		pLabel->setScaledContents(true);
+		pHLayout->addWidget(pLabel);
 
-		m_lstLabels << pLabel;
+		pWidget->move(0, -500);
+		lstWidgets << pWidget;
 	}
 
 	m_pDanMuCtrl->SetShowArea(0, 0, QSize(width(), height() / 2));//设置显示区域
 	m_pDanMuCtrl->SetRowNumber(3);//设置显示几行弹幕
-	m_pDanMuCtrl->SetLabels(m_lstLabels);
+	m_pDanMuCtrl->SetWidgets(lstWidgets);
 	m_pDanMuCtrl->Start();
 }
