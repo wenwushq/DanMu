08128565005441a0a72d777d6fbe4fa025df4224
wenwushq <41138799+wenwushq@users.noreply.github.com>
2020-10-14 11:06:25 +0800

    Update DanMuControl.h

--- DanMu/DanMuControl.h
+++ DanMu/DanMuControl.h
@@ -2,7 +2,7 @@
 
 #include <QObject>
 #include <QSize>
-#include <QLabel>
+#include <QWidget>
 #include <QMap>
 #include <QPropertyAnimation>
 
@@ -22,7 +22,7 @@ public:
 	void SetRowNumber(int nNumber);
 
 	//设置弹幕标签
-	void SetLabels(QList<QLabel*> lstLabels);
+	void SetWidgets(QList<QWidget*> lstWidgets);
 
 	void Start();//开始弹幕
 
@@ -30,7 +30,7 @@ private:
 	void CalcPerRowYPos();//计算出每一行的起始Y坐标
 	void GenAnimationByIndex(int nIndex);//生成动画
 	void GenAnimationByHeight(int nHeight);//生成动画
-	int GetLabelInRowHeight(QLabel* pLabel);//根据label的高度得到所在的哪一行
+	int GetWidgetInRowHeight(QWidget* pWidget);//根据widget的高度得到所在的哪一行
 
 private slots:
 	void slotGeometryAnimationValueChanged(const QVariant &varValue);//移动的动画值更改
@@ -39,15 +39,15 @@ private slots:
 
 private:
 	bool m_bLoop;//是否循环
-	int m_nShowAreaLeft, m_nShowAreaTop;//显示区域左右上角点
+	int m_nShowAreaLeft, m_nShowAreaTop;//显示区域左上角点
 	QSize m_sizeShowArea;//显示区域
 	int m_nRowNumber;//显示几行弹幕
-	QList<QLabel*> m_lstLabels;
+	QList<QWidget*> m_lstWidgets;
 
 	QList<int> m_lstnRowYPos;//每一行的起始Y坐标
 	int m_nDanMuSpace;//同一行两个弹幕间的间隙
-	QMap<QLabel*, QPropertyAnimation*> m_mapLabelOpacityAnim;
-	QMap<QLabel*, QPropertyAnimation*> m_mapLabelGeometryAnim;
+	QMap<QWidget*, QPropertyAnimation*> m_mapWidgetOpacityAnim;
+	QMap<QWidget*, QPropertyAnimation*> m_mapWidgetGeometryAnim;
 	QMap<QPropertyAnimation*, QGraphicsOpacityEffect*> m_mapOpacityAnimEffect;
 	int m_nDanMuIndex;//当前弹幕的索引
 	bool m_bStartSuccess;
